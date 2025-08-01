739e7baed2cadb78f50442d0cae536a49c363ca5
98937 <98937@LAPTOP-3DLHA77J>
2025-08-01 22:10:53 +0330

    Add GitHub Actions workflow and test project

---
+++ CiCdDemo.Tests/HelloTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using CiCdDemo;
+
+namespace CiCdDemo.Tests;
+
+public class HelloTests
+{
+    [Fact]
+    public void SayHello_ShouldReturnHelloWorld()
+    {
+        var result = Hello.SayHello();
+        Assert.Equal("Hello, World!", result);
+    }
+}
---
+++ ConsoleApp4/Hello.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CiCdDemo;
+
+public static class Hello
+{
+    public static string SayHello()
+    {
+        return "Hello, World!";
+    }
+}
